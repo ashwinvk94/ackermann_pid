80a46e3fe5cf350a2aa183a8f6e8e7dd777deb91
charan <pvcharankarthikeyan@gmail.com>
2019-10-10 11:16:38 -0400

    The source file of the pid controller

---
+++ app/pid.cpp
@@ -0,0 +1,64 @@
+/**
+ * @file pid.cpp
+ * @author Ashwin Vargheese Kuruttukulam
+ * @author Charan Karthikeyan
+ * @brief Defines a PID Controller that computes the
+ * heading angle for the target heading and point from
+ * the current heading and point.
+ * @Copyright "Copyright 2019 <Ashwin Vargheese Kuruttukulam>
+ * @Copyright "Copyright 2019 <Charan Karthikeyan>
+ */
+
+#include "pid.hpp"
+
+pid::pid(){
+
+}
+
+pid::~pid(){
+
+}
+
+void pid::pid(double kp, double ki, double kd){
+
+}
+double pid::compute(double feedback){
+
+}
+
+void pid::setSp(double setPoint){
+
+}
+
+void pid::setkp(double kp){
+
+}
+
+void pid::setki(double ki){
+
+}
+
+void pid::setkd(double kd){
+
+}
+
+double pid::getSp(){
+
+}
+double pid::getkp(){
+
+}
+
+double pid::getki(){
+
+}
+
+double pid::getkd(){
+
+}
+
+void pid::reset(){
+
+}
+
+
