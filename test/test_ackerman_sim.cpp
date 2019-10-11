87a3262fc2c8e11da7d6cbd43dde8b83570b7a4d
charan <pvcharankarthikeyan@gmail.com>
2019-10-11 18:28:35 -0400

    Added the test for ackerman simulation class

---
+++ test/test_ackerman_sim.cpp
@@ -0,0 +1,25 @@
+/**
+ * @file test_ackerman_sim.cpp
+ * @author Ashwin Varghese Kurutukullam
+ * @author Charan Karthikeyan
+ * @brief Testing cases for ackerman_controller simulation
+ *
+ * @Copyright "Copyright 2019 <Charan Karthikeyan>
+ * @Copyright "Copyright 2019 <Ashwin Varghese Kurutukullam>
+ */
+
+#include <gtest/gtest.h>
+
+#include "ackerman_sim.hpp"
+
+/**
+ * @brief Runs and tests the compute function in the ackerman_sim class for certain inputs
+ */
+TEST(ValidateAckermanSimulation,TestSimulationCompute){
+  ackerman_sim ack_sim;
+  EXPECT_EQ(ack_sim.compute(0,0,0),)
+
+}
+
+
+
