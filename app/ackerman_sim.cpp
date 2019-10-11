165b49d6ca77cf7aa8aa1f9723f9720daf3dc382
charan <pvcharankarthikeyan@gmail.com>
2019-10-10 20:41:39 -0400

    Function definition for ackerman simulation

---
+++ app/ackerman_sim.cpp
@@ -0,0 +1,24 @@
+/**
+ * @file ackerman_sim.cpp
+ * @author Ashwin Varghese Kuruttukulam
+ * @author Charan Karthikeyan
+ * @brief Declarations of the function to simulate the ackerman steering
+ * @Copyright "Copyright 2019 <Ashwin Varghese Kuruttukulam>
+ * @Copyright "Copyright 2019 <Charan Karthikeyan>
+ */
+
+#include"ackerman_sim.hpp"
+
+ackerman_sim::ackerman_sim(){
+
+}
+
+ackerman_sim::ackerman_sim(double dt){
+
+}
+
+double ackerman_sim::compute(double steerAnd,double rWheelVel,double lWheelVel){
+
+}
+
+
